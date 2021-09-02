0d6ffa45bc8926b67f4a40f7d09faedbddf1d480
Mia Chen <mia.chen.mtl@gmail.com>
2021-09-01 21:33:10 -0400

    Write README text

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const { createServer } = require('http');
-const { createHash } = require('crypto');
-const { moveCursor } = require('readline');
-const fs = require('fs');
+const { createServer } = require("http");
+const { createHash } = require("crypto");
+const { moveCursor } = require("readline");
+const fs = require("fs");
 
 const PORT = 8888;
-const WATCHED_FILENAME = 'index.html';
+const WATCHED_FILENAME = "index.html";
 const DEBOUNCE_INTERVAL = 50;
-const WEBSOCKET_MAGIC_STRING = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
+const WEBSOCKET_MAGIC_STRING = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 
-const SPINNER_CHARS = '|/—\\';
+const SPINNER_CHARS = "|/—\\";
 let spinnerCounter = 0;
 
 let connection; // FIXME: Allow multiple connections
@@ -16,7 +16,7 @@ let intervalId;
 let isConnected = false;
 let lastModified = fs.statSync(WATCHED_FILENAME).mtimeMs;
 
-const watcher = fs.watch('.', (eventType, filename) => {
+const watcher = fs.watch(".", (eventType, filename) => {
   // `fs.watch` is finicky. Recommended solution from thisdavej.com/how-to-watch-for-files-changes-in-node-js/
   // suggests looking solely at time modified with debounce interval, but even that
   // was buggy if only watching a single file. When editing on gedit on ubuntu, saving
@@ -43,10 +43,14 @@ const watcher = fs.watch('.', (eventType, filename) => {
         */
         const message = Buffer.from([0x81, 0x02, 0x3a, 0x29]);
         connection.write(message, () => {
-          console.log(`[${new Date().toLocaleTimeString()}]: Reload message sent to client.`);
+          console.log(
+            `[${new Date().toLocaleTimeString()}]: Reload message sent to client.`
+          );
         });
       } else {
-        console.log(`[${new Date().toLocaleTimeString()}]: ${WATCHED_FILENAME} updated, but nothing is connected!`);
+        console.log(
+          `[${new Date().toLocaleTimeString()}]: ${WATCHED_FILENAME} updated, but nothing is connected!`
+        );
       }
     }
   }
@@ -73,53 +77,53 @@ const pingClient = () => {
       // poomoji will be replaced by spinner when pong received
       // the browser WebSocket API automatically responds to pings by
       // ponging the same payload (but masked).
-      process.stdout.write('💩');
+      process.stdout.write("💩");
       moveCursor(process.stdout, -2, 0); // the emoji is 2 chars long
     });
   }
-}
+};
 
 const server = createServer((request, response) => {
-  if (request.url === '/') {
-    fs.readFile(filePath = WATCHED_FILENAME, function(error, content) {
+  if (request.url === "/") {
+    fs.readFile((filePath = WATCHED_FILENAME), function (error, content) {
       if (error) {
         console.error(error);
         response.end(error.message);
-      }
-      else {
+      } else {
         console.log(`Serving ${WATCHED_FILENAME}`);
-          response.writeHead(200, { 'Content-Type': 'text/html' });
-          response.end(content, 'utf-8');
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.end(content, "utf-8");
       }
     });
-  } else if (request.url === '/favicon.ico') {
+  } else if (request.url === "/favicon.ico") {
     // chrome makes separate request for favicon
     // see https://gist.github.com/kentbrew/763822
-    response.writeHead(200, {'Content-Type': 'image/x-icon'} );
+    response.writeHead(200, { "Content-Type": "image/x-icon" });
     response.end();
   } else {
-    response.writeHead(404, { 'Content-Type': 'text/plain' });
+    response.writeHead(404, { "Content-Type": "text/plain" });
     response.write(`${request.url} not found.`);
     response.end();
   }
 });
 
-server.on('upgrade', (req, socket) => {
-  console.log('Client requesting connection upgrade...');
-  socket.on('error', (event) => { console.error(`WebSocket error: ${event}`); });
-  socket.on('data', (data) => {
+server.on("upgrade", (req, socket) => {
+  console.log("Client requesting connection upgrade...");
+  socket.on("error", (event) => {
+    console.error(`WebSocket error: ${event}`);
+  });
+  socket.on("data", (data) => {
     if (data[0] === 0x8a && data[1] === 0x84) {
       // pong received
-      if (data.length !== 10) console.error('Incorrect pong payload length received!');
+      if (data.length !== 10)
+        console.error("Incorrect pong payload length received!");
       else {
         const mask = data.subarray(2, 6);
         const payload = data.subarray(6, 10);
-        const unmaskedText = Buffer
-          .from(
-            payload.map((byte, i) => byte ^ mask[i])
-            )
-          .toString();
-        if (unmaskedText !== 'ping') console.error('Wrong pong!');
+        const unmaskedText = Buffer.from(
+          payload.map((byte, i) => byte ^ mask[i])
+        ).toString();
+        if (unmaskedText !== "ping") console.error("Wrong pong!");
         else {
           process.stdout.write(SPINNER_CHARS.charAt(spinnerCounter % 4));
           moveCursor(process.stdout, -1, 0);
@@ -127,44 +131,51 @@ server.on('upgrade', (req, socket) => {
         }
       }
     } else if (data[0] === 0x88 && data[1] === 0x80 && data.length === 6) {
-      console.log('Client closing connection...');
+      console.log("Client closing connection...");
       connection.destroy();
       isConnected = false;
       clearInterval(intervalId);
     } else {
-      console.log(`Mysterious data received: ${data.toString()}`);
-      console.log(data);
+      console.log(`Unexpected data received: ${data.toString()}`);
     }
   });
-  socket.on('timeout', () => { console.log(`WebSocket timeout!`); });
-  socket.on('close', (hadError) => {
+  socket.on("timeout", () => {
+    console.log(`WebSocket timeout!`);
+  });
+  socket.on("close", (hadError) => {
     clearInterval(intervalId);
-    console.log(hadError ? 'Connection closed due to transmission error!' : 'Connection closed.');
-    console.log('=========================');
+    console.log(
+      hadError
+        ? "Connection closed due to transmission error!"
+        : "Connection closed."
+    );
+    console.log("=========================");
   });
 
-  const secWebSocketKey = req.headers['sec-websocket-key'];
-  const acceptKey = createHash('sha1')
+  const secWebSocketKey = req.headers["sec-websocket-key"];
+  const acceptKey = createHash("sha1")
     .update(secWebSocketKey + WEBSOCKET_MAGIC_STRING)
-    .digest('base64');
+    .digest("base64");
 
   socket.write(
-    'HTTP/1.1 101 Web Socket Protocol Handshake\r\n' +
-    'Upgrade: WebSocket\r\n' +
-    'Connection: Upgrade\r\n' +
-    `Sec-WebSocket-Accept: ${acceptKey}\r\n` +
-    '\r\n',
+    "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" +
+      "Upgrade: WebSocket\r\n" +
+      "Connection: Upgrade\r\n" +
+      `Sec-WebSocket-Accept: ${acceptKey}\r\n` +
+      "\r\n",
     () => {
-      console.log('Connection upgraded.\n');
+      console.log("Connection upgraded.\n");
       isConnected = true;
       intervalId = setInterval(pingClient, 5000);
       connection = socket;
-      process.stdout.write('\x1B[?25l'); // hides cursor
+      process.stdout.write("\x1B[?25l"); // hides cursor
     }
   );
 });
 
 server.listen(PORT, () => {
-  console.log(`[${new Date().toLocaleTimeString()}]: Server ready on localhost:${PORT}.`);
-  console.log('=========================');
+  console.log(
+    `[${new Date().toLocaleTimeString()}]: Server ready on localhost:${PORT}.`
+  );
+  console.log("=========================");
 });
